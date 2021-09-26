73a630634845472195709488b7609a5931f49c9f
Luppo <ondrej.sloup@protonmail.com>
2021-09-27 01:19:12 +0200

    First E2E version

--- cypress/integration/add-expensive-tvs-to-basket.ts
+++ cypress/integration/add-expensive-tvs-to-basket.ts
@@ -5,6 +5,39 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
-it('Goes to CZC.CZ. Navigate to TVs category. Add two most expensive TVs to the shopping cart', () => {
-    cy.visit('https://czc.cz/')
+/// <reference types="cypress" />
+
+
+context('Assertions', () => {
+    beforeEach(() => {
+        cy.visit('https://czc.cz/');
+        cy.clearCookies();
+      })
+
+    it('Navigate to TVs category. Add two most expensive TVs to the shopping cart', () => {
+        cy.get('.main-menu__title').contains('TV, audio, foto').click();
+        cy.get('.main-menu__submenu a').contains('Televize').click();
+
+        cy.url().should('include', '/televize/produkty');
+
+        cy.intercept('POST', '/dwr/call/plaincall/ProductListDwr.updateProductList.dwr').as('getItems')
+            cy.get('.nav-tile-switch a').contains('Nejdražší').click();
+        cy.wait('@getItems')
+        
+        cy.get('#tiles div.new-tile').each(($el, clicked = 0) => {
+            cy.wrap($el).within(() => {
+                cy.get('button').invoke('attr', 'data-buy-mode').then((status) => {
+                    if (status === "normal") {
+                        cy.get('button').click();   
+                        clicked += 1;
+                        
+                        cy.document().its('body').find('.buy-mode-product__item').contains('Pokračovat').click();
+                    }
+                });
+            })
+            if (clicked === 2) {
+                return false;
+            }
+        })
+    })
 })
