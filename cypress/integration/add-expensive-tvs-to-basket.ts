3e14d42b0d93f003be7de81fa2e6c71e76886203
Luppo <ondrej.sloup@protonmail.com>
2021-09-26 19:29:15 +0200

    Initial commit of cypress project

---
+++ cypress/integration/add-expensive-tvs-to-basket.ts
@@ -0,0 +1,10 @@
+// add-expensive-tvs-to-basket.ts created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+it('Goes to CZC.CZ. Navigate to TVs category. Add two most expensive TVs to the shopping cart', () => {
+    cy.visit('https://czc.cz/')
+})
